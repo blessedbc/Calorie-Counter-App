0ac3fca2708bc3ac1c8d87887d1d1c7723679b00
ikenna nwaka <inwaka39@gmail.com>
2025-06-06 19:24:58 +0200

    lunchNumberInputs

--- script.js
+++ script.js
@@ -37,4 +37,5 @@ function calculateCalories(e) {
   isError = false;
 
   const breakfastNumberInputs = document.querySelectorAll("#breakfast input[type='number']");
+  const lunchNumberInputs = document.querySelectorAll("#lunch input[type='number']");
 }
