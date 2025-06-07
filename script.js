5a7b0282303da80b1c0ab021518da89a9fbe8180
ikenna nwaka <inwaka39@gmail.com>
2025-06-07 18:55:58 +0200

    if

--- script.js
+++ script.js
@@ -49,4 +49,7 @@ function calculateCalories(e) {
     const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
     const budgetCalories = getCaloriesFromInputs([budgetNumberInput]);
 
+    if (isError) {
+    return;
+  }
 }
