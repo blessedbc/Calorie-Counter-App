71dcc2f7df4124591e3f4d6b72c5ac62c09d45df
ikenna nwaka <inwaka39@gmail.com>
2025-06-07 18:54:37 +0200

    budgetCalories

--- script.js
+++ script.js
@@ -47,4 +47,6 @@ function calculateCalories(e) {
     const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
     const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
     const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
+    const budgetCalories = getCaloriesFromInputs([budgetNumberInput]);
+
 }
