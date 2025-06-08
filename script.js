5d412e7cf63e9c58671561c8e68a902e914740ca
ikenna nwaka <inwaka39@gmail.com>
2025-06-08 20:32:37 +0200

    remainingCalories

--- script.js
+++ script.js
@@ -54,4 +54,5 @@ function calculateCalories(e) {
   }
 
   const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
+  const remainingCalories = budgetCalories - consumedCalories + exerciseCalories;
 }
