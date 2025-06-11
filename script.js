1a36c9a1dd03c6f73cdebfc33d2998ee89269e92
ikenna nwaka <inwaka39@gmail.com>
2025-06-11 18:46:14 +0200

    calories

--- script.js
+++ script.js
@@ -68,5 +68,18 @@ function calculateCalories(e) {
 }
 
 function getCaloriesFromInputs(list) {
+     let calories = 0;
 
+  for (const item of list) {
+    const currVal = cleanInputString(item.value);
+    const invalidInputMatch = isInvalidInput(currVal);
+
+    if (invalidInputMatch) {
+      alert(`Invalid Input: ${invalidInputMatch[0]}`);
+      isError = true;
+      return null;
+    }
+    calories += Number(currVal);
+  }
+  return calories;
 }
